e0f59f531dae3e3b0d3c3422ae68b921f689a8d7
Blackscure <Wekesabuyahi@gmail.com>
2020-04-20 12:13:54 +0300

    Private creted

--- src/main/java/models/CaeserCipherDecoder.java
+++ src/main/java/models/CaeserCipherDecoder.java
@@ -1,11 +1,18 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class CaeserCipherDecoder {
+public class CaesarCipherDecoder {
     private CaesarCipherEncoder EncoderDetails = new CaesarCipherEncoder("", 0);
 
     private String userText;
     private int decodeKey;
     private List<String> decodedOutput = new ArrayList<String>();
+
+
+    public CaesarCipherDecoder(String userText, int decodeKey) {
+        this.userText = userText;
+        this.decodeKey = decodeKey;
+    }
 }
