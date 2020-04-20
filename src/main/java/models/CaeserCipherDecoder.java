98aa19973b01778674dca0b9445be97d165a4bf8
Blackscure <Wekesabuyahi@gmail.com>
2020-04-20 11:29:20 +0300

    Private casear cipher encoder created and set to an new caeser cipher encoder

--- src/main/java/models/CaeserCipherDecoder.java
+++ src/main/java/models/CaeserCipherDecoder.java
@@ -1,4 +1,11 @@
 package models;
 
+import java.util.ArrayList;
+
 public class CaeserCipherDecoder {
+    private CaesarCipherEncoder EncoderDetails = new CaesarCipherEncoder("", 0);
+
+    private String userText;
+    private int decodeKey;
+    private List<String> decodedOutput = new ArrayList<String>();
 }
