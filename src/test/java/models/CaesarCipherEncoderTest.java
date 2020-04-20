2a9e516e057b245c7213010009d14f06be23fd99
Blackscure <Wekesabuyahi@gmail.com>
2020-04-20 10:43:48 +0300

    Asseert and Test imported

--- src/test/java/models/CaesarCipherEncoderTest.java
+++ src/test/java/models/CaesarCipherEncoderTest.java
@@ -1,4 +1,13 @@
 package models;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CaesarCipherEncoderTest {
+    @Test
+    public void newCaesarCipherObject_instantiatesApproprietly() throws Exception {
+        CaesarCipherDecoder testCaesarCipherDecoder = new CaesarCipherDecoder("A", 1);
+        assertEquals(true, testCaesarCipherDecoder instanceof CaesarCipherDecoder);
+    }
 }
