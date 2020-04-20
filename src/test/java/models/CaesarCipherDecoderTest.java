e78a83da94b9a271a9cf6a99c14e5a44b2239776
Blackscure <Wekesabuyahi@gmail.com>
2020-04-20 12:15:50 +0300

    public craeted

--- src/test/java/models/CaesarCipherDecoderTest.java
+++ src/test/java/models/CaesarCipherDecoderTest.java
@@ -1,4 +1,10 @@
-package models;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CaesarCipherDecoderTest {
+    @Test
+    public void newCaesarCipherObject_instantiatesCorrectly() throws Exception {
+        CaesarCipherDecoder testCaesarCipherDecoder = new CaesarCipherDecoder("A", 1);
+        assertEquals(true, testCaesarCipherDecoder instanceof CaesarCipherDecoder);
+    }
 }
